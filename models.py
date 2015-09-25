0f3ffae1c77c13359327b8dc76617a17eea97a9d
maratkanov-a <a.s.maratkanov@gmail.com>
2015-09-25 21:36:33 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class User(models.Model):
+    avatar = models.ImageField()
+
+    meta = User
+
+
+class Question(models.Model):
+    header = models.CharField(max_length=100)
+    body = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    date = models.DateField()
+    tags = models.ForeignKey(Tags)
+    rating = models.IntegerField(default=0)
+
+class Answer(models.Model):
+    body = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
+    date = models.DateField()
+    flag = models.BooleanField(default=False)
+    rating = models.IntegerField(default=0)
+
+class Tags(models.Model):
+    text = models.CharField(max_length=20)
