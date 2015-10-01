e1369fef344dd7870c851b20d351f33f51b3b67f
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-01 21:44:05 +0300

    added login/logout url

--- ask_project/urls.py
+++ ask_project/urls.py
@@ -5,7 +5,7 @@ from ask_app.views import *
 urlpatterns = [
 
     url(r'^$', AskAppTemplateView.as_view(), name='home-page'),
-    # registration; change views
+    # registration
     url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
 
