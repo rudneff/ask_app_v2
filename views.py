de05b930c7ffac73285907e620be5b7dd5d1f753
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-01 21:43:35 +0300

    added login/logout url

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, DeleteView, DetailView, ListView, UpdateView
 
+class AskAppTemplateView(TemplateView):
+    template_name = 'base.html'
