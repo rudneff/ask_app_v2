cf06ab9397637d4738e8c7761c92b273de0ea299
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-02 22:04:12 +0300

    added new views, and templates

--- views.py
+++ views.py
@@ -2,14 +2,41 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, DeleteView, DetailView, ListView, UpdateView
 from ask_app.models import *
 
-
+# class for template
 class AskAppTemplateView(TemplateView):
     template_name = 'base.html'
 
-
+# classes for working with Question models
 class QuestionListView(ListView):
     model = Question
 
 
 class QuestionDetailView(DetailView):
     model = Question
+
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name_suffix = '_update_form'
+
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+
+# classes for working with Answers models
+class AnswerListView(ListView):
+    model = Answer
+
+
+class AnswerDetailView(DetailView):
+    model = Answer
+
+
+class AnswerUpdateView(UpdateView):
+    model = Answer
+    template_name_suffix = '_update_form'
+
+
+class AnswerDeleteView(DeleteView):
+    model = Answer
+
