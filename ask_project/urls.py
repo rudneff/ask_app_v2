3875132eff7b4703e47c4ccc710f1223838fd4b6
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-05 18:43:13 +0300

    changed urls, fixed forms for login, registration

--- ask_project/urls.py
+++ ask_project/urls.py
@@ -11,21 +11,24 @@ urlpatterns = [
 
     # question pages
     url(r'^question/(?P<pk>\d+)/$', QuestionDetailView.as_view(), name='question-page'),
+    url(r'^question/(?P<pk>\d+)/update/$', UserUpdateView.as_view(), name='question-update-page'),
     url(r'^questions/$', QuestionListView.as_view(), name='question-list-page'),
-    url(r'^question-create/$', QuestionCreateView.as_view(), name='question-create-page'),
-    url(r'^question-delete/$', QuestionDeleteView.as_view(), name='question-delete-page'),
+    url(r'^question/create/$', QuestionCreateView.as_view(), name='question-create-page'),
+    url(r'^question/(?P<pk>\d+)/delete/$', QuestionDeleteView.as_view(), name='question-delete-page'),
 
     # answers pages
     url(r'^answer/(?P<pk>\d+)/$', AnswerDetailView.as_view(), name='answer-page'),
+    url(r'^answer/(?P<pk>\d+)/update/$', UserUpdateView.as_view(), name='answer-update-page'),
     url(r'^answers/$', AnswerListView.as_view(), name='answer-list-page'),
-    url(r'^answer-create/$', AnswerCreateView.as_view(), name='answer-create-page'),
-    url(r'^answer-delete/$', AnswerDeleteView.as_view(), name='answer-delete-page'),
+    url(r'^answer/create/$', AnswerCreateView.as_view(), name='answer-create-page'),
+    url(r'^answer/(?P<pk>\d+)/delete/$', AnswerDeleteView.as_view(), name='answer-delete-page'),
 
     # user pages
     url(r'^user/(?P<pk>\d+)/$', UserDetailView.as_view(), name='user-page'),
+    url(r'^user/(?P<pk>\d+)/update/$', UserUpdateView.as_view(), name='user-update-page'),
     url(r'^users/$', UserListView.as_view(), name='user-list-page'),
-    url(r'^user-create/$', UserCreateView.as_view(), name='user-create-page'),
-    url(r'^user-delete/$', UserDeleteView.as_view(), name='user-delete-page'),
+    url(r'^user/create/$', UserCreateView.as_view(), name='user-create-page'),
+    url(r'^user/(?P<pk>\d+)/delete/$', UserDeleteView.as_view(), name='user-delete-page'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
