c088caf965c82bae01a8a686f067d28a275e9f5c
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-05 20:04:20 +0300

    added styles to questions page, aslo fixed urls

--- ask_project/urls.py
+++ ask_project/urls.py
@@ -5,7 +5,7 @@ from ask_app.views import *
 urlpatterns = [
 
     url(r'^$', AskAppTemplateView.as_view(), name='home-page'),
-    # registration
+    # authentication
     url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
 
