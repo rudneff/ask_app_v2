984687dae5410179090c8e9678e0ddf4b062bd3f
maratkanov-a <a.s.maratkanov@gmail.com>
2015-10-12 13:52:29 +0300

    fixed question style

--- ask_project/urls.py
+++ ask_project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    url(r'^$', AskAppTemplateView.as_view(), name='home-page'),
+    url(r'^$', QuestionListView.as_view(), name='home-page'),
     # authentication
     url(r'^login/', 'django.contrib.auth.views.login', name='login-page'),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-page'),
